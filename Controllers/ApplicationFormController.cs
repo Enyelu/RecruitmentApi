6b936c2ce9a4c9bdee54966f792c2c3b110b20c6
Chukwuebuka Enyelu <careers.joseph@yahoo.com>
2023-10-07 20:35:40 +0100

    add required controllers

---
+++ Controllers/ApplicationFormController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecruitmentApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ApplicationFormController : ApiBaseController
+    {
+        public ApplicationFormController()
+        {
+        }
+
+        [HttpGet()]
+        //[Produces("application/json", Type = typeof())]
+        public async Task<IActionResult> Test()
+        {
+            return Ok("TESTED");
+        }
+    }
+}
