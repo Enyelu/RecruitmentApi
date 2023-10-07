7e415c57cc6a1bb409928979fb65a5d1bc578a5d
Chukwuebuka Enyelu <careers.joseph@yahoo.com>
2023-10-07 19:33:34 +0100

    configure kestrel server to handle requests

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+var host = new WebHostBuilder()
+            .UseKestrel()
+            .ConfigureServices(services =>
+            {
+                services.AddCors();
+                services.AddRouting();
+                services.AddControllers();
+                services.AddAntiforgery();
+                
+            })
+            .Configure(app =>
+            {
+                app.UseCors(options => options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+                app.UseHttpsRedirection();
+                app.UseRouting(); // Enable routing
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                });
+            })
+            .Build();
+
+host.Run();
