d4030558fab4200150e0380ac832066cedb3b768
Chukwuebuka Enyelu <careers.joseph@yahoo.com>
2023-10-08 16:31:46 +0100

    add preview tab CRUD

---
+++ RecruitmentDomain/Models/PreviewDto.cs
@@ -0,0 +1,16 @@
+﻿using RecruitmentDomain.Entities;
+
+namespace RecruitmentDomain.Models
+{
+    public class PreviewDto
+    {
+        public WorkflowDto Workflow { get; set; }
+        public ProgramDetailsDto ProgramDetail { get; set; }
+        public ApplicationFormDto ApplicationForm { get; set; }
+    }
+
+    public class PreviewListDto
+    {
+        public List<PreviewDto> PreviewList { get; set; }
+    }
+}
